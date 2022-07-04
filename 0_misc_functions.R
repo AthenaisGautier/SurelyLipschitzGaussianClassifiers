5576a6b261268f06f9690de014af8b2c56b64c7f
AthenaisGautier <athena.gau@gmail.com>
2022-07-04 18:00:41 +0200

    Update 0_misc_functions.R

--- 0_misc_functions.R
+++ 0_misc_functions.R
@@ -45,23 +45,31 @@ basis_fun_value <- function(location_multi_index, multi_index_nodes, n_nodes, di
 }
 
 
-#### predict_probability_fullcalc
-# A function that takes a (or several) new directions, values for epsilon (weights), the domain bounds and number of nodes, and predicts the probability at considered new_loc by computing everything
+#### field_predict
+# A function that takes a (or several) new locations, values for epsilon (weights), the domain bounds and number of nodes, and predicts the probability at considered new_loc by computing everything
 ####
-predict_probability_fullcalc <- function(new_loc, weights, dim,
-                                         domain_bounds,
-                                         n_nodes){
-  delta_i <- get_delta_i(domain_bounds, n_nodes)
-  location_multi_index <- coord_to_multi_index(new_loc, delta_i, domain_bounds[, 1])
-  basis_fun_value <- basis_fun_value(location_multi_index=location_multi_index, 
-                                     n_nodes, dim, delta_i)
-  
-  GP_values <- sapply(seq(nrow(weights)), function(i){
-    eval_gp(basis_fun_value, weights[i, ])
-  })
-  sigmoid_values <- 1/(1+exp(-GP_values)) 
-  df <- data.frame(x=t(new_loc), GP=GP_values, sigmoid=sigmoid_values)
-  return(df)
+
+field_predict <- function(new_coord, weights, data, name_index, domain_bounds, dim,
+                          n_nodes){
+  # Useful quantities regarding the nodes we consider
+  delta_i <- get_discretization_step(domain_bounds, n_nodes)
+  multi_index_nodes <- index_to_multiindex(seq(1, n_nodes^dim), n_nodes, dim)
+  coord_nodes <- t(multiindex_to_coord(multi_index_nodes, delta_i, domain_bounds[, 1]))
+  colnames(coord_nodes) <- name_index
+  new_coord_loc <- new_coord[, name_index, drop=FALSE]
+  location_multi_index <- coord_to_multiindex(coord=new_coord_loc, 
+                                              delta_i, lower_bound = domain_bounds[, 1])
+  fun_value <- basis_fun_value(location_multi_index=location_multi_index,
+                               multi_index_nodes=multi_index_nodes,
+                               n_nodes, dim, delta_i)
+  GP_val <- fun_value%*%  t(samp)
+  res <- data.frame(ix = seq(nrow(new_coord)), 
+                    GP=c(GP_val), 
+                    real=c(sapply(seq(nrow(samp)), FUN=function(k){
+                      rep(k, nrow(new_coord))})))
+  res <- cbind(new_coord[res$ix, , drop=FALSE], res[, -c(1)])
+  res$prob <- 1/(1+exp(-res$GP))
+  return(res)
 }
 
 #### constraints_matrix
