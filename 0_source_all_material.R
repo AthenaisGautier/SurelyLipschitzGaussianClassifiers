9485ef838bf790fb3b7a17f772798beff117ede4
AthenaisGautier <athena.gau@gmail.com>
2022-07-04 15:59:31 +0200

    Update 0_source_all_material.R

--- 0_source_all_material.R
+++ 0_source_all_material.R
@@ -1,4 +1,4 @@
 # We only need to call this script to source all functions created
 source(".0_misc_functions.R")
-
+source(".0_coord_index_multiindex.R")
 source(".0_priors_hyperparameters_Matern.R")
